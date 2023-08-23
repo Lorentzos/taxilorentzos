09742544217b0ba8c6f9475ca8fefc59de6ab64a
Lorentzos <141639738+Lorentzos@users.noreply.github.com>
2023-08-23 14:55:21 +0300

    Update internet.js

--- js/internet.js
+++ js/internet.js
@@ -1,45 +1,58 @@
-/*!
-  * Coded by Lorentzos
-  * Encrypted in LorentzosEncryptor
-  * Licensed under MIT (https://lorentzos.servers) -- private accses
-  */
+// This is a simplified example for educational purposes.
+// In a real-world scenario, use proper encryption libraries and secure communication protocols.
 
-var _0x284a90 = _0x4e57;
-function _0x4e57(_0x5040d3, _0x311bec) {
-    var _0x1e0b2d = _0x1e0b();
-    return _0x4e57 = function(_0x4e57da, _0x436819) {
-        _0x4e57da = _0x4e57da - 0xa0;
-        var _0x3ef06a = _0x1e0b2d[_0x4e57da];
-        return _0x3ef06a;
-    }
-    ,
-    _0x4e57(_0x5040d3, _0x311bec);
+// Function to encrypt data (insecure for demonstration purposes)
+function encryptData(data) {
+  // In a real-world application, you would use a proper encryption library
+  // and securely manage encryption keys.
+  return btoa(data);
 }
-function _0x1e0b() {
-    var _0x532532 = ['364882cUpCRm', 'online', '3589488IFIXlQ', 'nowifi.html', '10910dLDxIo', 'href', 'onLine', '5366736FcfpWS', '9768ebKgrv', 'index.html', '67059VinyKG', 'addEventListener', '6457gFdANE', '608LRMhiI', 'location', '53XSiEqI', '6zuKUOi', '281585VyynNL'];
-    _0x1e0b = function() {
-        return _0x532532;
-    }
-    ;
-    return _0x1e0b();
+
+// Function to decrypt data (insecure for demonstration purposes)
+function decryptData(encryptedData) {
+  // In a real-world application, you would use a proper decryption library
+  // and securely manage decryption keys.
+  return atob(encryptedData);
 }
-(function(_0x1962f9, _0x548def) {
-    var _0x3d7342 = _0x4e57
-      , _0x470ea9 = _0x1962f9();
-    while (!![]) {
-        try {
-            var _0x545d55 = -parseInt(_0x3d7342(0xa2)) / 0x1 * (parseInt(_0x3d7342(0xad)) / 0x2) + parseInt(_0x3d7342(0xa7)) / 0x3 + -parseInt(_0x3d7342(0xac)) / 0x4 + -parseInt(_0x3d7342(0xa4)) / 0x5 * (parseInt(_0x3d7342(0xa3)) / 0x6) + -parseInt(_0x3d7342(0xa5)) / 0x7 + -parseInt(_0x3d7342(0xa0)) / 0x8 * (-parseInt(_0x3d7342(0xaf)) / 0x9) + parseInt(_0x3d7342(0xa9)) / 0xa * (parseInt(_0x3d7342(0xb1)) / 0xb);
-            if (_0x545d55 === _0x548def)
-                break;
-            else
-                _0x470ea9['push'](_0x470ea9['shift']());
-        } catch (_0x43cba3) {
-            _0x470ea9['push'](_0x470ea9['shift']());
-        }
-    }
-}(_0x1e0b, 0xa97c2));
-!navigator[_0x284a90(0xab)] && (window['location']['href'] = _0x284a90(0xa8));
-window[_0x284a90(0xb0)](_0x284a90(0xa6), function() {
-    var _0x5ebb94 = _0x284a90;
-    window[_0x5ebb94(0xa1)][_0x5ebb94(0xaa)] = _0x5ebb94(0xae);
-});
+
+// Create a cookie with encrypted data
+function createEncryptedCookie(name, value) {
+  const encryptedValue = encryptData(value);
+  document.cookie = `${name}=${encryptedValue}; path=/`;
+}
+
+// Function to send encrypted data to the server
+function sendDataToServer(encryptedData) {
+  // In a real-world application, you would use HTTPS and a secure API endpoint
+  const serverUrl = 'https://taxithassoslimenariapotos.com/server';
+  
+  // Simulate sending encrypted data to the server (not secure)
+  fetch(serverUrl, {
+    method: 'POST',
+    body: encryptedData,
+    headers: {
+      'Content-Type': 'application/octet-stream', // Adjust content type as needed
+    },
+  }).then(response => {
+    // Handle server response here
+  }).catch(error => {
+    // Handle errors here
+  });
+}
+
+// Main function
+function main() {
+  const dataToEncrypt = 'Some sensitive information';
+  
+  // Create an encrypted cookie with the data
+  createEncryptedCookie('encryptedData', dataToEncrypt);
+  
+  // Encrypt the data again (for demonstration purposes)
+  const encryptedData = encryptData(dataToEncrypt);
+  
+  // Send encrypted data to the server
+  sendDataToServer(encryptedData);
+}
+
+// Call the main function when the script loads
+main();
